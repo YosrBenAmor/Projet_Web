23a6d8527edc9576b3e2be6ce925c53b8f60e158
YosrBenAmor <100871594+YosrBenAmor@users.noreply.github.com>
2022-11-21 15:00:06 +0100

    yosr_part

--- template_front/EVENEMENT_PARTICIPATION/Views/supprimerParticipation.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('../Controller/ParticipationC.php');
-	$participationC=new participationC();
-	$participationC->supprimerParticipation($_GET["id_part"]);
-	header('Location:afficherListeParticipation.php');
-	
-?>
